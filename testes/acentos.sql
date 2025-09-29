ca44f8ae605f6d5c68e87071568f921ad6059de8
nunezada08 <vitor.nunes3013@gmail.com>
2025-09-29 11:41:58 -0300

    feat: adiciona pasta teste e o arquivo acentos.sql

---
+++ testes/acentos.sql
@@ -0,0 +1,13 @@
+CREATE TABLE testes (
+    id SERIAL PRIMARY KEY,
+    nome VARCHAR(100) NOT NULL
+);
+
+INSERT INTO testes (nome)
+VALUES
+('João'),
+('Stéfanny'),
+('Ãngelo'),
+('Márcio'),
+('Letícia'),
+('Júlia');
